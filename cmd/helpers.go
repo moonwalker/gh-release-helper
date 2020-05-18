96ada02d3d9164e02b43410ead81685d9afd7739
speier <kalman.speier@gmail.com>
2020-05-18 16:26:45 +0200

    improved git helpers and get last 10

--- cmd/helpers.go
+++ cmd/helpers.go
@@ -8,24 +8,41 @@ import (
 )
 
 func gitrepo() string {
-	p := run("git", "config", "--get", "remote.origin.url")
-	return run("basename", "-s", ".git", p)
-}
+	p, err := run("git", "config", "--get", "remote.origin.url")
+	if err != nil {
+		log.Fatal(p)
+	}
 
-func gitlog() string {
-	tag := run("git", "describe", "--tags", "--abbrev=0")
-	return run("git", "log", "--pretty=format:- %H %s", tag+"...")
-}
-
-func run(name string, args ...string) string {
-	cmd := exec.Command(name, args...)
-	cmd.Env = os.Environ()
-
-	out, err := cmd.CombinedOutput()
-	res := strings.TrimSpace(string(out))
+	res, err := run("basename", "-s", ".git", p)
 	if err != nil {
 		log.Fatal(res)
 	}
 
 	return res
+}
+
+func gitlog() string {
+	t, err := run("git", "describe", "--tags", "--abbrev=0")
+	if err != nil {
+		t = "-10"
+	} else {
+		t = t + "..."
+	}
+
+	res, err := run("git", "log", "--pretty=format:- %H %s", t)
+	if err != nil {
+		log.Fatal(res)
+	}
+
+	return res
+}
+
+func run(name string, args ...string) (string, error) {
+	cmd := exec.Command(name, args...)
+	cmd.Env = os.Environ()
+
+	out, err := cmd.CombinedOutput()
+	res := strings.TrimSpace(string(out))
+
+	return res, err
 }
